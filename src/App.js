d409db77344fc685ac5b06585d66927b3065cb46
AntCamper <152764140+AntCamper@users.noreply.github.com>
2024-11-24 22:10:09 -0800

    Added a new grid background done with CSS started working on a Floating thumbnail with a torodial and porodial effect to show off projects from class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ProfileContainer from './components/ProfileContainer';
+import FloatingThumbnails from './components/FloatingThumbnails';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
       <div style={{
-        backgroundImage: `url(${require('./images/windows_xp_bliss-wide.png')})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
         width: '100vw',
         height: '100vh'
       }}>
